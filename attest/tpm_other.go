355782cbf9ec8f827cd50deb176efa2063a12fdd
Brandon Weeks <bweeks@google.com>
2019-10-08 06:11:40 +0000

    Fix building without cgo

---
+++ attest/tpm_other.go
@@ -0,0 +1,62 @@
+// Copyright 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+// +build !linux !windows
+
+package attest
+
+type platformTPM struct {
+	// interf TPMInterface
+}
+
+func probeSystemTPMs() ([]probedTPM, error) {
+	return nil, nil
+}
+
+func openTPM(tpm probedTPM) (*TPM, error) {
+	return nil, nil
+}
+
+func (t *platformTPM) tpmVersion() TPMVersion {
+	return TPMVersionAgnostic
+}
+
+func (t *platformTPM) close() error {
+	return nil
+}
+
+func (t *platformTPM) info() (*TPMInfo, error) {
+	return nil, nil
+}
+
+func (t *platformTPM) loadAIK(opaqueBlob []byte) (*AIK, error) {
+	return nil, nil
+}
+
+func (t *platformTPM) eks() ([]EK, error) {
+	return nil, nil
+}
+
+func (t *platformTPM) newAIK(opts *AIKConfig) (*AIK, error) {
+	return nil, nil
+}
+
+func (t *platformTPM) pcrs(alg HashAlg) ([]PCR, error) {
+	return nil, nil
+}
+
+func (t *platformTPM) measurementLog() ([]byte, error) {
+	return nil, nil
+}
+
