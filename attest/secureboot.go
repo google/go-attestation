ee5bb94c434f76ee4cdc3556bbb5ed08bab98453
Tom D <40675700+twitchy-jsonp@users.noreply.github.com>
2021-05-10 12:11:58 -0700

    WIP processing image load events (#216)

--- attest/secureboot.go
+++ attest/secureboot.go
@@ -56,7 +56,19 @@ type SecurebootState struct {
 	// PostSeparatorAuthority describes the use of a secure-boot key to authorize
 	// the execution of a binary after the separator.
 	PostSeparatorAuthority []x509.Certificate
+
+	// DriverLoadSourceHints describes the origin of boot services drivers.
+	// This data is not tamper-proof and must only be used as a hint.
+	DriverLoadSourceHints []DriverLoadSource
 }
+
+// DriverLoadSource describes the logical origin of a boot services driver.
+type DriverLoadSource uint8
+
+const (
+	UnknownSource DriverLoadSource = iota
+	PciMmioSource
+)
 
 // ParseSecurebootState parses a series of events to determine the
 // configuration of secure boot on a device. An error is returned if
@@ -78,14 +90,16 @@ func ParseSecurebootState(events []Event) (*SecurebootState, error) {
 	// - No UEFI debugger was attached.
 
 	var (
-		out           SecurebootState
-		seenSeparator bool
-		seenAuthority bool
-		seenVars      = map[string]bool{}
+		out            SecurebootState
+		seenSeparator7 bool
+		seenSeparator2 bool
+		seenAuthority  bool
+		seenVars       = map[string]bool{}
+		driverSources  [][]internal.EFIDevicePathElement
 	)
 
 	for _, e := range events {
-		if e.Index != 7 {
+		if e.Index != 7 && e.Index != 2 {
 			continue
 		}
 
@@ -93,97 +107,154 @@ func ParseSecurebootState(events []Event) (*SecurebootState, error) {
 		if err != nil {
 			return nil, fmt.Errorf("unrecognised event type: %v", err)
 		}
-
 		digestVerify := e.digestEquals(e.Data)
-		switch et {
-		case internal.Separator:
-			if seenSeparator {
-				return nil, fmt.Errorf("duplicate separator at event %d", e.sequence)
-			}
-			seenSeparator = true
-			if !bytes.Equal(e.Data, []byte{0, 0, 0, 0}) {
-				return nil, fmt.Errorf("invalid separator data at event %d: %v", e.sequence, e.Data)
-			}
-			if digestVerify != nil {
-				return nil, fmt.Errorf("invalid separator digest at event %d: %v", e.sequence, digestVerify)
-			}
 
-		case internal.EFIAction:
-			if string(e.Data) == "UEFI Debug Mode" {
-				return nil, errors.New("a UEFI debugger was present during boot")
-			}
-			return nil, fmt.Errorf("event %d: unexpected EFI action event", e.sequence)
+		switch e.Index {
+		case 7:
+			switch et {
+			case internal.Separator:
+				if seenSeparator7 {
+					return nil, fmt.Errorf("duplicate separator at event %d", e.sequence)
+				}
+				seenSeparator7 = true
+				if !bytes.Equal(e.Data, []byte{0, 0, 0, 0}) {
+					return nil, fmt.Errorf("invalid separator data at event %d: %v", e.sequence, e.Data)
+				}
+				if digestVerify != nil {
+					return nil, fmt.Errorf("invalid separator digest at event %d: %v", e.sequence, digestVerify)
+				}
 
-		case internal.EFIVariableDriverConfig:
-			v, err := internal.ParseUEFIVariableData(bytes.NewReader(e.Data))
-			if err != nil {
-				return nil, fmt.Errorf("failed parsing EFI variable at event %d: %v", e.sequence, err)
-			}
-			if _, seenBefore := seenVars[v.VarName()]; seenBefore {
-				return nil, fmt.Errorf("duplicate EFI variable %q at event %d", v.VarName(), e.sequence)
-			}
-			seenVars[v.VarName()] = true
-			if seenSeparator {
-				return nil, fmt.Errorf("event %d: variable %q specified after separator", e.sequence, v.VarName())
-			}
+			case internal.EFIAction:
+				if string(e.Data) == "UEFI Debug Mode" {
+					return nil, errors.New("a UEFI debugger was present during boot")
+				}
+				return nil, fmt.Errorf("event %d: unexpected EFI action event", e.sequence)
 
-			if digestVerify != nil {
-				return nil, fmt.Errorf("invalid digest for variable %q on event %d: %v", v.VarName(), e.sequence, digestVerify)
-			}
+			case internal.EFIVariableDriverConfig:
+				v, err := internal.ParseUEFIVariableData(bytes.NewReader(e.Data))
+				if err != nil {
+					return nil, fmt.Errorf("failed parsing EFI variable at event %d: %v", e.sequence, err)
+				}
+				if _, seenBefore := seenVars[v.VarName()]; seenBefore {
+					return nil, fmt.Errorf("duplicate EFI variable %q at event %d", v.VarName(), e.sequence)
+				}
+				seenVars[v.VarName()] = true
+				if seenSeparator7 {
+					return nil, fmt.Errorf("event %d: variable %q specified after separator", e.sequence, v.VarName())
+				}
 
-			switch v.VarName() {
-			case "SecureBoot":
-				if len(v.VariableData) != 1 {
-					return nil, fmt.Errorf("event %d: SecureBoot data len is %d, expected 1", e.sequence, len(v.VariableData))
+				if digestVerify != nil {
+					return nil, fmt.Errorf("invalid digest for variable %q on event %d: %v", v.VarName(), e.sequence, digestVerify)
 				}
-				out.Enabled = v.VariableData[0] == 1
-			case "PK":
-				if out.PlatformKeys, out.PlatformKeyHashes, err = v.SignatureData(); err != nil {
-					return nil, fmt.Errorf("event %d: failed parsing platform keys: %v", e.sequence, err)
-				}
-			case "KEK":
-				if out.ExchangeKeys, out.ExchangeKeyHashes, err = v.SignatureData(); err != nil {
-					return nil, fmt.Errorf("event %d: failed parsing key exchange keys: %v", e.sequence, err)
-				}
-			case "db":
-				if out.PermittedKeys, out.PermittedHashes, err = v.SignatureData(); err != nil {
-					return nil, fmt.Errorf("event %d: failed parsing signature database: %v", e.sequence, err)
-				}
-			case "dbx":
-				if out.ForbiddenKeys, out.ForbiddenHashes, err = v.SignatureData(); err != nil {
-					return nil, fmt.Errorf("event %d: failed parsing forbidden signature database: %v", e.sequence, err)
-				}
-			}
 
-		case internal.EFIVariableAuthority:
-			a, err := internal.ParseUEFIVariableAuthority(bytes.NewReader(e.Data))
-			if err != nil {
-				// Workaround for: https://github.com/google/go-attestation/issues/157
-				if err == internal.ErrSigMissingGUID {
-					// Versions of shim which do not carry
-					// https://github.com/rhboot/shim/commit/8a27a4809a6a2b40fb6a4049071bf96d6ad71b50
-					// have an erroneous additional byte in the event, which breaks digest
-					// verification. If verification failed, we try removing the last byte.
-					if digestVerify != nil && len(e.Data) > 0 {
-						digestVerify = e.digestEquals(e.Data[:len(e.Data)-1])
+				switch v.VarName() {
+				case "SecureBoot":
+					if len(v.VariableData) != 1 {
+						return nil, fmt.Errorf("event %d: SecureBoot data len is %d, expected 1", e.sequence, len(v.VariableData))
 					}
-				} else {
-					return nil, fmt.Errorf("failed parsing EFI variable authority at event %d: %v", e.sequence, err)
+					out.Enabled = v.VariableData[0] == 1
+				case "PK":
+					if out.PlatformKeys, out.PlatformKeyHashes, err = v.SignatureData(); err != nil {
+						return nil, fmt.Errorf("event %d: failed parsing platform keys: %v", e.sequence, err)
+					}
+				case "KEK":
+					if out.ExchangeKeys, out.ExchangeKeyHashes, err = v.SignatureData(); err != nil {
+						return nil, fmt.Errorf("event %d: failed parsing key exchange keys: %v", e.sequence, err)
+					}
+				case "db":
+					if out.PermittedKeys, out.PermittedHashes, err = v.SignatureData(); err != nil {
+						return nil, fmt.Errorf("event %d: failed parsing signature database: %v", e.sequence, err)
+					}
+				case "dbx":
+					if out.ForbiddenKeys, out.ForbiddenHashes, err = v.SignatureData(); err != nil {
+						return nil, fmt.Errorf("event %d: failed parsing forbidden signature database: %v", e.sequence, err)
+					}
 				}
-			}
-			seenAuthority = true
-			if digestVerify != nil {
-				return nil, fmt.Errorf("invalid digest for authority on event %d: %v", e.sequence, digestVerify)
-			}
-			if !seenSeparator {
-				out.PreSeparatorAuthority = append(out.PreSeparatorAuthority, a.Certs...)
-			} else {
-				out.PostSeparatorAuthority = append(out.PostSeparatorAuthority, a.Certs...)
+
+			case internal.EFIVariableAuthority:
+				a, err := internal.ParseUEFIVariableAuthority(bytes.NewReader(e.Data))
+				if err != nil {
+					// Workaround for: https://github.com/google/go-attestation/issues/157
+					if err == internal.ErrSigMissingGUID {
+						// Versions of shim which do not carry
+						// https://github.com/rhboot/shim/commit/8a27a4809a6a2b40fb6a4049071bf96d6ad71b50
+						// have an erroneous additional byte in the event, which breaks digest
+						// verification. If verification failed, we try removing the last byte.
+						if digestVerify != nil && len(e.Data) > 0 {
+							digestVerify = e.digestEquals(e.Data[:len(e.Data)-1])
+						}
+					} else {
+						return nil, fmt.Errorf("failed parsing EFI variable authority at event %d: %v", e.sequence, err)
+					}
+				}
+				seenAuthority = true
+				if digestVerify != nil {
+					return nil, fmt.Errorf("invalid digest for authority on event %d: %v", e.sequence, digestVerify)
+				}
+				if !seenSeparator7 {
+					out.PreSeparatorAuthority = append(out.PreSeparatorAuthority, a.Certs...)
+				} else {
+					out.PostSeparatorAuthority = append(out.PostSeparatorAuthority, a.Certs...)
+				}
+
+			default:
+				return nil, fmt.Errorf("unexpected event type in PCR7: %v", et)
 			}
 
-		default:
-			return nil, fmt.Errorf("unexpected event type: %v", et)
+		case 2:
+			switch et {
+			case internal.Separator:
+				if seenSeparator2 {
+					return nil, fmt.Errorf("duplicate separator at event %d", e.sequence)
+				}
+				seenSeparator2 = true
+				if !bytes.Equal(e.Data, []byte{0, 0, 0, 0}) {
+					return nil, fmt.Errorf("invalid separator data at event %d: %v", e.sequence, e.Data)
+				}
+				if digestVerify != nil {
+					return nil, fmt.Errorf("invalid separator digest at event %d: %v", e.sequence, digestVerify)
+				}
+
+			case internal.EFIBootServicesDriver:
+				if !seenSeparator2 {
+					imgLoad, err := internal.ParseEFIImageLoad(bytes.NewReader(e.Data))
+					if err != nil {
+						return nil, fmt.Errorf("failed parsing EFI image load at boot services driver event %d: %v", e.sequence, err)
+					}
+					dp, err := imgLoad.DevicePath()
+					if err != nil {
+						return nil, fmt.Errorf("failed to parse device path for driver load event %d: %v", e.sequence, err)
+					}
+					driverSources = append(driverSources, dp)
+				}
+			}
 		}
+	}
+
+	// Compute driver source hints based on the EFI device path observed in
+	// EFI Boot-services driver-load events.
+sourceLoop:
+	for _, source := range driverSources {
+		// We consider a driver to have originated from PCI-MMIO if any number
+		// of elements in the device path [1] were PCI devices, and are followed by
+		// an element representing a "relative offset range" read.
+		// In the wild, we have typically observed 4-tuple device paths for such
+		// devices: ACPI device -> PCI device -> PCI device -> relative offset.
+		//
+		// [1]: See section 9 of the UEFI specification v2.6 or greater.
+		var seenPCI bool
+		for _, e := range source {
+			// subtype 0x1 corresponds to a PCI device (See: 9.3.2.1)
+			if e.Type == internal.HardwareDevice && e.Subtype == 0x1 {
+				seenPCI = true
+			}
+			// subtype 0x8 corresponds to "relative offset range" (See: 9.3.6.8)
+			if seenPCI && e.Type == internal.MediaDevice && e.Subtype == 0x8 {
+				out.DriverLoadSourceHints = append(out.DriverLoadSourceHints, PciMmioSource)
+				continue sourceLoop
+			}
+		}
+		out.DriverLoadSourceHints = append(out.DriverLoadSourceHints, UnknownSource)
 	}
 
 	if !out.Enabled {
