85b86ebf388348e7da7432bcc880b05772c3e978
Alex Wu <wuale@google.com>
2021-08-31 13:42:02 -0700

    Invert 'notspi' build tag to 'tspi'
    This change allows users to specify TPM1.2 support rather than remove it.
    go-attestation will build without needing Trousers/TSPI support.
    The flip-side of this is that TPM1.2 does not just work; TPM1.2 users need to
    include the `tspi` build tag.

--- attest/key_linux.go
+++ attest/key_linux.go
@@ -12,7 +12,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-// +build linux,!gofuzz,cgo,!notspi
+// +build linux,!gofuzz,cgo,tspi
 
 package attest
 
